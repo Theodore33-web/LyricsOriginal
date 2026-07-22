31b3b5cd6b8a5df9ce6706cd37cd48a0e7629e45
Theodore33-web <theomennesson140807@gmail.com>
2026-07-22 22:54:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-const APP_VERSION = "v1.1.api";
+const APP_VERSION = "v1.1.30";
 
 // Crée un bouton "Afficher plus" avec un style forcé en JS,
 // identique à 100% partout où il est utilisé (bibliothèque, écoutes
